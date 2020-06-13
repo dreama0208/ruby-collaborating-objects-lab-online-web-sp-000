5262ea1eb7b8236f52836acbe3aa47759136a41a
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-13 21:28:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,11 @@
 class MP3Importer
+
+  attr_accessor :test_music_path
+
+  def initialize(test_music_path)
+
+  end
+
   def import(list_of_filenames)
     list_of_filenames.each {|fileneme| Song.new_by_filename(filename)}
   end
